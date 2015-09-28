350db8e36de4f4377f1a1b64f83932f6626e9d91
peter nankunda <peternankunda@gmail.com>
2015-09-28 12:08:44 +0300

    Added the new changes

--- book.py
+++ book.py
@@ -1,7 +1,10 @@
 authorr={}
+print "1 - Enter book"
+print "2 - Find book"
+menue = int(raw_input("Choose any number"))
 
 def enterbook():
-
+    global authorr
     for i in range(2):
 
         Title = raw_input("Enter book Title")
@@ -10,10 +13,16 @@ def enterbook():
     return authorr
 
 def findbook():
-    title = enterbook()
+    #title = enterbook()
     Title = raw_input("Enter book Title")
-    author = title[Title]
+    author = authorr[Title]
     return author
 
+if menue==1:
+    enterbook()
 
-print findbook()
+elif menue==2:
+    print findbook()
+
+
+#print findbook()
