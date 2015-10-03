83cc11a7699cacc6bf92a7878e906fd297a7c5f6
peter nankunda <peternankunda@gmail.com>
2015-10-03 17:40:23 +0300

    changed the dictionary name

--- book.py
+++ book.py
@@ -1,28 +1,30 @@
-authorr={}
-print "1 - Enter book"
-print "2 - Find book"
-menue = int(raw_input("Choose any number"))
+shelf={}
+
+
+def showmenue():
+    print "1 - Enter book"
+    print "2 - Find book"
+    menue = int(raw_input("Choose any number"))
+    if menue==1:
+        enterbook()
+    elif menue==2:
+        findbook()
 
 def enterbook():
-    global authorr
-    for i in range(2):
+    Title = raw_input("Enter book Title")
+    Author = raw_input("Enter author")
+    shelf[Title] = Author
+    showmenue()
 
-        Title = raw_input("Enter book Title")
-        Author = raw_input("Enter author")
-        authorr[Title] = Author
-    return authorr
+
 
 def findbook():
-    #title = enterbook()
     Title = raw_input("Enter book Title")
-    author = authorr[Title]
-    return author
-
-if menue==1:
-    enterbook()
-
-elif menue==2:
-    print findbook()
+    author = shelf[Title]
+    print author
 
 
-#print findbook()
+
+showmenue()
+
+
