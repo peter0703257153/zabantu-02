e884fb5ca1cb4f83c0b20397705b6988384d36bd
peter nankunda <peternankunda@gmail.com>
2015-10-19 08:34:22 +0300

    improved the code to use lists

--- book.py
+++ book.py
@@ -1,14 +1,29 @@
-shelf={}
+
 
 class Book(object):
-    def __init__(self,Title,Author):
-        self.Title=Title
-        self.Author=Author
-        shelf[self.Title]=self.Author
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+        #shelf[self.Title]=self.Author
 
     def __repr__(self):
-        #return self.Title + self.Author
-        return shelf
+        return self.title + self.author
+        #return shelf
+
+
+class Shelf(object):
+     def __init__(self):
+         self.books=[]
+
+     def add(self,book):
+        self.books.append(book)
+
+     def find(self,title):
+         for book in self.books:
+            if book.title==title:
+                return book
+
+book_shelf=Shelf()
 
 def showmenue():
     print "1 - Enter book"
@@ -20,16 +35,18 @@ def showmenue():
         findbook()
 
 def enterbook():
-    Title = raw_input("Enter book Title")
-    Author = raw_input("Enter author")
-    book = Book(Title,Author)
+    title = raw_input("Enter book Title")
+    author = raw_input("Enter author")
+    book = Book(title,author)
+    book_shelf.add(book)
     showmenue()
 
 
 
 def findbook():
-    Title = raw_input("Enter book Title")
-    print shelf[Title]
+    title = raw_input("Enter book Title")
+    book = book_shelf.find(title)
+    print book
     showmenue()
 
 
